d8a34f87ea59ed25832460bb2f9f6181c587581b
rtiwari6 <106265482+rtiwari6@users.noreply.github.com>
2024-04-02 04:12:22 -0700

    Create common.h

---
+++ Sprint9/raviUpload/common.h
@@ -0,0 +1,66 @@
+#include "mpfs_hal/mss_hal.h"
+#include "drivers/mss/mss_gpio/mss_gpio.h"
+#include "drivers/fpga-ip/CoreGPIO/core_gpio.h"
+#include "drivers/mss/mss_mmuart/mss_uart.h"
+#include "drivers/fpga-ip/CoreSPI/core_spi.h"
+#include "drivers/mss/mss_spi/mss_spi.h"
+
+extern uint16_t display_init;
+extern uint8_t NOOP;
+extern uint8_t A;
+extern uint8_t B;
+extern uint8_t C;
+extern uint8_t D;
+extern uint8_t E;
+extern uint8_t F;
+extern uint8_t G;
+extern uint8_t H;
+extern uint8_t I;
+extern uint8_t J;
+extern uint8_t K;
+extern uint8_t L;
+extern uint8_t M;
+extern uint8_t N;
+extern uint8_t O;
+extern uint8_t P;
+extern uint8_t Q;
+extern uint8_t R;
+extern uint8_t S;
+extern uint8_t T;
+extern uint8_t U;
+extern uint8_t W;
+extern uint8_t X;
+extern uint8_t Y;
+extern uint8_t Z;
+extern uint8_t zero;
+extern uint8_t one;
+extern uint8_t two;
+extern uint8_t three;
+extern uint8_t four;
+extern uint8_t five;
+extern uint8_t six;
+extern uint8_t seven;
+extern uint8_t eight;
+extern uint8_t nine;
+extern uint8_t dp;
+extern uint8_t digit;
+extern uint8_t digit1;
+extern uint8_t digit2;
+extern uint8_t digit3;
+extern uint8_t digit4;
+extern uint8_t digit5;
+extern uint8_t digit6;
+extern uint8_t digit7;
+extern uint8_t digit8;
+extern uint8_t decode_mode;
+extern uint8_t intensity;
+extern uint8_t scan_limit;
+extern uint8_t shutdown;
+extern uint8_t disp_test;
+
+
+#define UART0
+#define UART1
+#define UART4
+
+
